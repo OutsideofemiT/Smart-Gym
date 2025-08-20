b2cb548477faffda37ad0db683216faddc4a0ed5
EngineerMoore <alexiashalise@gmail.com>
2025-08-20 17:09:29 -0500

    Updated AddItemsModal inputs to reflect API request needs

--- src/components/admin-dashboard/inventory-management/AddItemsModal.tsx
+++ src/components/admin-dashboard/inventory-management/AddItemsModal.tsx
@@ -2,6 +2,8 @@ import type { ModalProps } from "../../../types/InventoryManagement.interface";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import { FcAddRow } from "react-icons/fc";
+import { GrFormSubtract } from "react-icons/gr";
 
 const AddItemsModal: React.FC<ModalProps> = ({ show, setShow }) => {
   const handleClose = () => setShow(false);
@@ -16,22 +18,45 @@ const AddItemsModal: React.FC<ModalProps> = ({ show, setShow }) => {
           <Modal.Title>Add Inventory Items</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
+          <Form className="add-items-input-entry">
+            <Form.Group
+              className="mb-3 item-name-input"
+              controlId="exampleForm.ControlInput1"
+            >
+              <Form.Label>Item Name</Form.Label>
               <Form.Control
-                type="email"
-                placeholder="name@example.com"
+                type="text"
+                placeholder="Protein Meal Bar"
                 autoFocus
               />
             </Form.Group>
             <Form.Group
-              className="mb-3"
+              className="mb-3 item-price-input"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="text" placeholder="8.00" autoFocus />
             </Form.Group>
+            <Form.Group
+              className="mb-3 item-quantity-input"
+              controlId="exampleForm.ControlInput1"
+              item-quantity-input
+            >
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control type="number" placeholder="10" autoFocus />
+            </Form.Group>
+            <Form.Text
+              className="text-muted add-entry-button"
+              title="Add Entry"
+            >
+              <FcAddRow />
+            </Form.Text>
+            <Form.Text
+              className="text-muted delete-entry-button"
+              title="Delete Entry"
+            >
+              <GrFormSubtract />
+            </Form.Text>
           </Form>
         </Modal.Body>
         <Modal.Footer>
