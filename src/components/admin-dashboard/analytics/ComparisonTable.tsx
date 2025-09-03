fd88e825ea1a67adf8dcba184c15c4867d217b23
EngineerMoore <alexiashalise@gmail.com>
2025-09-03 09:16:09 -0500

    Analytics Established end to end connection for class attendance metric
    admin has the flexibility to see data for a range of years or select a specific year
    
    the comparison table title is always visible while the user scrolls

--- src/components/admin-dashboard/analytics/ComparisonTable.tsx
+++ src/components/admin-dashboard/analytics/ComparisonTable.tsx
@@ -25,30 +25,32 @@ const ComparisonTable: React.FC<ComparisonTableProps> = ({ title, data }) => {
   }, [data]);
 
   return (
-    <div className="comparison-table overflow-auto">
-      <h3>{title}</h3>
-      <table>
-        <thead>
-          <tr>
-            <th key="no-label"></th>
-            {headers.map((header) => (
-              <th key={header}>{header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((entry) => {
-            return (
-              <tr>
-                <td>{entry.timePoint}</td>
-                {headers.map((header) => (
-                  <td key={header}>{entry[header]}</td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div className="comparison-table">
+      <h3 className="comparison-table-title">{title}</h3>
+      <div className="comparison-table-scroll overflow-auto">
+        <table className="comparison-table-data">
+          <thead>
+            <tr>
+              <th key="no-label"></th>
+              {headers.map((header) => (
+                <th key={header}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((entry) => {
+              return (
+                <tr>
+                  <td>{entry.timePoint}</td>
+                  {headers.map((header) => (
+                    <td key={header}>{entry[header]}</td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
